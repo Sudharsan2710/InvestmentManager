87dae36aecae528bb0a2ebc1ff2ba88186cf2952
Sudh3564 <Sudh3564@CSL0708.global.chainsys.com>
2024-06-28 18:02:25 +0530

    Update

---
+++ InvestmentManager/src/main/java/com/chainsys/investmentmanager/InvestmentManagerApplication.java
@@ -0,0 +1,13 @@
+package com.chainsys.investmentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentManagerApplication.class, args);
+	}
+
+}
---
+++ InvestmentManager/src/main/java/com/chainsys/investmentmanager/model/User.java
@@ -0,0 +1,55 @@
+package com.chainsys.investmentmanager.model;
+
+public class User {
+	 private int userid;
+	    private String username;
+	    private String password;
+	    private String email;
+	    private String contact;
+
+	   
+	    public int getUserid() {
+	        return userid;
+	    }
+
+	    public void setUserid(int userid) {
+	        this.userid = userid;
+	    }
+
+	  
+	    public String getUsername() {
+	        return username;
+	    }
+
+	    public void setUsername(String username) {
+	        this.username = username;
+	    }
+
+	    // Getter and setter methods for password
+	    public String getPassword() {
+	        return password;
+	    }
+
+	    public void setPassword(String password) {
+	        this.password = password;
+	    }
+
+	    // Getter and setter methods for email
+	    public String getEmail() {
+	        return email;
+	    }
+
+	    public void setEmail(String email) {
+	        this.email = email;
+	    }
+
+	    // Getter and setter methods for contact
+	    public String getContact() {
+	        return contact;
+	    }
+
+	    public void setContact(String contact) {
+	        this.contact = contact;
+	    }
+
+}
